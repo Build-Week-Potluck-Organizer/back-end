3969704e040322a63fcb9fc7d5405c9aeed4c1c2
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-11-12 22:28:28 -0500

    users seed, env file added

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,13 +1,16 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+
+const hashCount = parseInt(process.env.HASH_COUNT) || 8;
+
+exports.seed = function (knex) {
+  return knex("users")
+    .del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("users").insert([
+        { username: "fulano", password: bcrypt.hashSync("password", hashCount) },
+        { username: "fulana", password: bcrypt.hashSync("password", hashCount) },
       ]);
     });
 };
